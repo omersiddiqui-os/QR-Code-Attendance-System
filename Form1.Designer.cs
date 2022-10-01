8d841e73147e1c916114a2e33ba16c7f3f2cba0a
Muhammad Omer Siddiqui <omersiddiqui67@gmail.com>
2022-10-01 22:22:21 +0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace WebCam_Barcode_Scanner
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(179, 265);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 28);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Stop";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 265);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 28);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(267, 246);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(291, 304);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(309, 351);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(309, 351);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "WebCam Barcode Scanner";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MessagingToolkit.Barcode;
+using BasselTech_CamCapture;
+using System.Data.SqlClient;
+
+namespace WebCam_Barcode_Scanner
+{
+    public partial class Form1 : Form
+    {
+        Camera cam;
+        Timer t;
+        BackgroundWorker worker;
+        Bitmap CapImage;
+        string con_string = "Data Source=OMER;Initial Catalog=qr_codes;Integrated Security=True";
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            t = new Timer();
+            cam = new Camera(pictureBox1);
+            worker = new BackgroundWorker();
+
+            worker.DoWork += Worker_DoWork;
+            t.Tick += T_Tick;
+            t.Interval = 1;
+        }
+
+        private void T_Tick(object sender, EventArgs e)
+        {
+            CapImage = cam.GetBitmap();
+            if (CapImage != null && !worker.IsBusy)
+                worker.RunWorkerAsync();
+        }
+
+        private void Worker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            BarcodeDecoder decoder = new BarcodeDecoder();
+            try
+            {
+                string decoded_text = decoder.Decode(CapImage).Text;
+                SqlConnection connection = new SqlConnection(con_string);
+                SqlCommand command = connection.CreateCommand();
+
+                connection.Open();
+                command.CommandText = "INSERT INTO dbo.log (employee_id) VALUES(@employee_id);";
+                command.Parameters.AddWithValue("@employee_id", decoded_text);
+                if (command.ExecuteNonQuery() > 0)
+                {
+                    MessageBox.Show("Attendance was successfully recorded for ID: " + decoded_text);
+                }
+
+                connection.Close();
+            }
+            catch (SqlException)
+            {
+                MessageBox.Show("Please make sure you scanned a correct QR Code!");
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                cam.Start();
+                t.Start();
+                button2.Enabled = true;
+                button1.Enabled = false;
+            }
+            catch(Exception ex)
+            {
+                cam.Stop();
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            t.Stop();
+            cam.Stop();
+            button2.Enabled = false;
+            button1.Enabled = true;
+        }
+    }
+}
